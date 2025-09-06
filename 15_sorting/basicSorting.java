2172bfc2a30223f3a8c3f0362acaecddf3662f3e
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-09-06 13:10:21 +0530

    refactor sorting methods to use swap function and improve readability

--- 15_sorting/basicSorting.java
+++ 15_sorting/basicSorting.java
@@ -4,12 +4,11 @@ public class basicSorting {
         for (int turn = 0; turn < arr.length - 1; turn++) {
             for (int j = 0; j < arr.length - 1 - turn; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    swap(arr, j, j + 1);
                 }
             }
         }
+        printArray(arr);
     }
 
     public static void selectionSort(int[] arr) {
@@ -20,10 +19,40 @@ public class basicSorting {
                     minPosition = j;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[minPosition];
-            arr[minPosition] = temp;
         }
+        printArray(arr);
+    }
+
+    public static void selectionSortBackward(int arr[]) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int biggest = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[biggest])
+                    biggest = j;
+            }
+            swap(arr, biggest, i);
+        }
+        printArray(arr);
+    }
+
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i], prev = i - 1;
+            // finding out correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // insertion
+            arr[prev +  1] = curr;
+        }
+        printArray(arr);
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void printArray(int[] arr) {
@@ -34,9 +63,6 @@ public class basicSorting {
 
     public static void main(String[] args) {
         int[] arr = { 5, 4, 1, 3, 2 };
-        // bubbleSort(arr);
-        selectionSort(arr);
-
-        printArray(arr);
+        insertionSort(arr);
     }
 }
